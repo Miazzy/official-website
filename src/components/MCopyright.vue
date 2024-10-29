6203754d315eceaef0050ca7b423bf108f3ebd96
miazzy <qq695926920@163.com>
2024-10-29 15:19:40 +0800

    fix: 移动端首页底部箭头换成最新的，要有闪烁的动画效果。

--- src/components/MCopyright.vue
+++ src/components/MCopyright.vue
@@ -2,7 +2,7 @@
     <!-- footer-container -->
     <div class="footer-container">
         <div class="image-wrapper flex-row" @click="handleClickDown">
-            <img class="label" src="../assets/images/arrow_down_grey.png" />
+            <img class="label" src="../assets/images/arrow_down_mobile.png" />
         </div>
         <div class="text-wrapper flex-row">
             <span class="text">Copyright ©渔光物联(www.ygwl.net) &nbsp; | &nbsp; 蜀ICP备16026089号</span>
@@ -19,9 +19,37 @@ const handleClickDown = () => {
 }
 
 onMounted(() => {
-
+    const img = document.querySelector('.image-wrapper img.label');
+    img.classList.add('flashing');
 });
 </script>
 <style lang="less" scoped>
 @import '@/assets/style/base.less';
+
+.image-wrapper img {
+    transition: all 0.3s ease;
+
+    &.flashing {
+        animation: flashing 0.5s ease infinite;
+    }
+
+    &:hover {
+        animation: none;
+    }
+}
+
+@keyframes flashing {
+    0% {
+        transform: translateY(0);
+        opacity: 1;
+    }
+    50% {
+        transform: translateY(5px);
+        opacity: 0.5;
+    }
+    100% {
+        transform: translateY(0);
+        opacity: 1;
+    }
+}
 </style>
