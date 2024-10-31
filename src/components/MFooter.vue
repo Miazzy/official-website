e02417a41ad9b5f7a14ca3fcd615028ac0634255
miazzy <qq695926920@163.com>
2024-10-31 13:39:29 +0800

    fix: footer 样式优化；

--- src/components/MFooter.vue
+++ src/components/MFooter.vue
@@ -1,22 +1,26 @@
 <template>
-    <!-- footer-container -->
-    <div class="footer-container flex-col">
-        <div class="footer-wrapper flex-row justify-between">
-            <div class="company-wrapper flex-col justify-between">
-                <img class="company-code-image"
-                    src="../assets/images/mobile_qr_code.png" />
-                <span class="company-code-text">扫一扫，关注渔光物联</span>
-            </div>
-            <div class="contact-wrapper flex-row">
-                <div class="contact-content flex-row justify-between">
-                    <span
-                        class="contact-content-paragraph">Tel：028-86168758<br />E-mail：WEIH02&#64;tongwei.com<br />Addr.：四川省成都市高新区天府大道588号通威国际中心</span>
-                </div>
-            </div>
+  <!-- footer-container -->
+  <div class="footer-container flex-col">
+    <div class="footer-wrapper flex-row justify-between">
+      <div class="company-wrapper flex-col justify-between">
+        <img class="company-code-image" src="../assets/images/mobile_qr_code.png" />
+        <span class="company-code-text">扫一扫，关注渔光物联</span>
+      </div>
+      <div class="contact-wrapper flex-row">
+        <div class="contact-content flex-row justify-between">
+          <span
+            class="contact-content-paragraph">Tel：028-86168758<br />E-mail：WEIH02&#64;tongwei.com<br />Addr.：四川省成都市高新区天府大道588号通威国际中心</span>
         </div>
-        <div class="line-box flex-col"></div>
-        <span class="text-copyright">Copyright ©渔光物联（www.ygwl.net） 蜀ICP备16026089号</span>
+        <div class="contact-image-text flex-row justify-between">
+          <span class="icon iconfont image tel icondianhua"></span>
+          <span class="icon iconfont image email iconyouxiang"></span>
+          <span class="icon iconfont image addr iconzhengqimenhu_dizhixinxi1"></span>
+        </div>
+      </div>
     </div>
+    <div class="line-box flex-col"></div>
+    <span class="text-copyright">Copyright ©渔光物联（www.ygwl.net） 蜀ICP备16026089号</span>
+  </div>
 </template>
 <script setup>
 import { onMounted } from 'vue';
@@ -37,7 +41,7 @@ onMounted(() => {
   .footer-wrapper {
     width: 71.74vw;
     height: 11.6vw;
-    margin: 3.73vw 0 0 14.13vw;
+    margin: 3.73vw 0 0 13.13vw;
 
     .company-wrapper {
       width: 16vw;
@@ -45,9 +49,9 @@ onMounted(() => {
       margin-top: 0.27vw;
 
       .company-code-image {
-        width: 8.54vw;
-        height: 8.54vw;
-        margin-left: 3.74vw;
+        width: 8.50vw;
+        height: 8.50vw;
+        margin-left: 5.5vw;
       }
 
       .company-code-text {
@@ -69,7 +73,7 @@ onMounted(() => {
       position: relative;
       width: 53.74vw;
       height: 11.6vw;
-      margin: -0.75vw 0 0 6vw;
+      margin: -0.75vw 0 0 8.75vw;
 
       .contact-content {
         width: 53.74vw;
@@ -99,17 +103,48 @@ onMounted(() => {
   }
 
   .text-copyright {
-    width: 45.74vw;
+    width: 100vw;
     height: 1.87vw;
     overflow-wrap: break-word;
     color: #d9d9d9;
     font-size: 1.6vw;
-    font-family: SourceHanSansCN-Normal;
+    font-family: SourceHanSansCN;
     font-weight: normal;
-    text-align: left;
+    text-align: center;
     white-space: nowrap;
     line-height: 1.87vw;
     margin: 2vw auto 2.13vw auto;
+  }
+}
+
+.contact-image-text {
+  position: absolute;
+  width: 3vw;
+  height: 15vw;
+  left: -4vw;
+  top: 0.8vw;
+
+  .image {
+    position: absolute;
+    color: #fefefe;
+    transform: scale(0.5);
+
+    &.email {
+      width: 1.5vw;
+      height: 1.5vw;
+      margin-top: 4.75vw;
+    }
+
+    &.tel {
+      width: 1.5vw;
+      height: 1.5vw;
+    }
+
+    &.addr {
+      width: 1.5vw;
+      height: 1.5vw;
+      margin-top: 9.55vw;
+    }
   }
 }
 </style>
